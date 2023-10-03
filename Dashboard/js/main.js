5dcf3581c7f53ca765d7071eb0b6183ce4e59101
pinilDissanayaka <pinildissanayaka@gmail.com>
2023-10-03 09:10:30 +0530

    crop diseases

--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -7,3 +7,18 @@
     });    
 })(jQuery);
 
+function previewFile(input){
+    var file = $("input[type=file]").get(0).files[0];
+
+    if(file){
+        var reader = new FileReader();
+
+        reader.onload = function(){
+            $(".img-thumbnail").attr("src", reader.result);
+        }
+
+        reader.readAsDataURL(file);
+    }
+}
+
+
